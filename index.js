2cf7194a7b274e8e7d0f6697cbde4df972387224
joffy13 <nikikrokha@gmail.com>
2022-07-10 00:17:25 +0300

    added a funct cut string

--- index.js
+++ index.js
@@ -1,93 +1,11 @@
-//функция принимает 1 арг в виде масива который состоит из обьектов
-//вернуть сука тру
-// const names = [
-//     {name:"matvey",suka:false},
-//     {name:"Misha",suka:false},
-//     {name:"Oleg",suka:true}
-// ]
-// const nameSearch = (arg) =>{
-//     return arg.filter((suk) => suk.suka)
-// }
-// console.log(nameSearch(names))
 
-
-
-// const persons = [
-//     { name: "matvey", suka: true },
-//     { name: "Misha", suka: false },
-//     { name: "Oleg", suka: true },
-//     { name: "matvey", suka: true },
-//     { name: "Misha", suka: false },
-//     { name: "Oleg", suka: true },
-//     { name: "matvey", suka: false, dolbaeb:true},
-//     { name: "Misha", suka: false },
-//     { name: "Oleg", suka: true },
-// ]
-
-// const search = (argument) => {
-//     return argument.forEach(element => {
-//         console.log(element?.dolbaeb??'huita')
-//     });
-// }
-// search(persons)
-
-    
-
-const huina = [
-    { name: "matvey", suka: true },
-    { name: "Misha", suka: false, szbolda: true},
-    { name: "Oleg", suka: true, xuitaIzPodKonia: true },
-    { name: "matvey", suka: true ,dima: false},
-    { name: "Misha", suka: false ,igor: 'lox'},
-    { name: "Oleg", suka: true },
-    { name: "matvey", suka: false, dolbaeb: true, szbolda: true},
-    { name: "Misha", suka: false, szbolda228: true },
-    { name: "Oleg", suka: true, szbolda: true }
-]
-// const resp = (arg) =>{
-//     return arg.forEach(element => {
-//         console.log((element?.xuitaIzPodKonia??element.name)&&(element?.szbolda??element.name))
-//     });
-// }
-// resp(huina)
-
-// const rest = (arg) => {
-//     return arg.filter(element => element.name === "Oleg" && element?.xuitaIzPodKonia === true)[0].name
-// }
-// console.log(rest(huina))
-// const mass = [2, 4, 1]
-
-// const rest = (arg) => {
-//     return arg.reduce((acc, curVa, index)=>{
-//        if(mass.some(massH => index === massH)){
-//         return acc = [...acc, curVa]
-//        } 
-//        else{
-//         return acc
-//        }
-//     },[])
-// }
-// // const gdflsj = [1, 2, 3]
-// // gdflsj.reduce()
-// console.log(rest (huina))
-
-// //расчитать расход денег на километраж учитывая
-// //цена топлива, расход,дистанция
-
-// const value = (CostFuel, FuelPerKm, distance) => {
-//     return 'В общем выходит ' + FuelPerKm / 100 * distance * CostFuel + ' гривен'
-// }
-// console.log(value(40, 13, 250))
-
-
-const stringer = 'Я пошёл НАхуй, потомУ что не любою ЧЛены'
-
-const isUp = (arg, index) => {
-    return arg[index].toUpperCase() === arg.charAt(index)
+const cutsring = (red, n) =>{
+    let result = []
+    for (let index = 0; index < red.length; index+= n) {
+        // console.log(red[index])
+        result.push(red.substr(index, n))
+        console.log(index)
+    }
+    return result
 }
-console.log(isUp(stringer, 6))
-
-
-
-
-
+console.log(cutsring('наслаждение',2))
